df95dcf8fd4dd4a51dac4ab3d5b0a2f3d0ea7b84
Brian Washington Jr <bw49078@gmail.com>
2026-07-18 00:12:14 -0500

    Enemy Spawner no longer spams out enemies

--- Assets/Scripts/SpawnEnemyTest.cs
+++ Assets/Scripts/SpawnEnemyTest.cs
@@ -2,18 +2,21 @@ using UnityEngine;
 using Fusion;
 using Unity.VisualScripting;
 
-public class SpawnEnemyTest : MonoBehaviour
+public class SpawnEnemyTest : NetworkBehaviour
 {
     [SerializeField] private PlayerSpawner _spawner;
     [SerializeField] private NetworkPrefabRef _enemy;
     [SerializeField] private Transform _spawnPosition;
     [SerializeField] private int _spawnCountMax;
 
-    private int _spawnCount;
+    private int _spawnCount { get; set; }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && _spawnCount < _spawnCountMax)
+        if (!Object.HasStateAuthority) return;
+        if (_spawnCount >= _spawnCountMax) return;
+
+        if (other.CompareTag("Player"))
         {
             _spawner.SpawnEnemy(_enemy, _spawnPosition.position);
             _spawnCount++;
