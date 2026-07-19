d388ee26da9de526be60312b4f8265282afd66fc
Brian Washington Jr <bw49078@gmail.com>
2026-07-18 20:29:32 -0500

    Player can now attack enemies

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,9 @@ public class PlayerController : NetworkBehaviour
     private NetworkCharacterController _cc;
     private CharacterController _characterController;
     public GameObject _camera;
+    private readonly Collider[] hitColliders = new Collider[10];
+    Vector3 GetHitboxCenter() => transform.position + (transform.forward * 1.5f);
+    Vector3 GetHitboxHalfExtents() => new Vector3(1.5f, 1.5f, 2.0f) / 2f;
     private void Awake()
     {
         _cc = GetComponent<NetworkCharacterController>();
@@ -57,9 +60,29 @@ public class PlayerController : NetworkBehaviour
 
         _cc.Move(move);
     }
-
     private void Attack()
     {
-        Debug.Log($"{_cc.Id} attacked");
+        Vector3 hitboxCenter = GetHitboxCenter();
+        Vector3 halfExtents = GetHitboxHalfExtents();
+
+        int numColliders = Physics.OverlapBoxNonAlloc(hitboxCenter, halfExtents, hitColliders, transform.rotation);
+
+        for (int i = 0; i < numColliders; i++)
+        {
+            Collider hit = hitColliders[i];
+
+            if (hit.CompareTag("Enemy") && hit.TryGetComponent<NetworkObject>(out var enemy))
+            {
+                Debug.Log($"Enemy {enemy.Id} was attacked by player {_cc.Id}");
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Matrix4x4 rotationMatrix = Matrix4x4.TRS(GetHitboxCenter(), transform.rotation, Vector3.one);
+        Gizmos.matrix = rotationMatrix;
+        Gizmos.DrawWireCube(Vector3.zero, GetHitboxHalfExtents() * 2f);
     }
 }
